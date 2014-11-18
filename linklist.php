dd969c6065193faf46be6fec3667cb7b409aee9f
ivy <597055914@qq.com>
2014-11-18 22:34:08 +0800

    update method of class linklist

--- linklist.php
+++ linklist.php
@@ -8,7 +8,7 @@ class node{
 	private $data;
 	private $next;
 
-	function __construct($Data=NULL,$Next=NULL){
+	function __construct($Data=NULL,node $Next=NULL){
 		$this->data=$Data;
 		$this->next=$next;
 	}
@@ -25,7 +25,7 @@ class node{
 		return $this->data=$Data;
 	}
 
-	function setNext($Next){
+	function setNext(node $Next){
 		$this->next=$Next;
 	}
 }
@@ -38,7 +38,78 @@ class linkList{
 	private $head;
 
 	function __construct(argument){
-		$head=new node();
+		$this->head=new node();
+	}
+
+	function create1($n){
+
+	}
+
+	function create2($n){
+
+	}
+
+	function get($i){
+
+	}
+
+	function insert($i,$x){
+		$p=$this->head;
+		$j=0;
+
+		while($p!=NULL && $j<$i-1){
+			$p=$p->getNext();
+			$j+=1;
+		}
+
+		if($j>$i || $p=null){
+			return -1; //插入位置非法
+		}
+
+		$s=new node($x);
+		if($i==0){
+			//表头
+			$s->setNext($p);
+			$p=$s;
+		}else{
+			$s->setNext($p->getNext());
+			$p->setNext($s);
+		}
+	}
+
+	function remnove($i){
+
+	}
+
+	function indexOf($x){
+
+	}
+
+	function clear(){
+		$this->head->setData(NULL);
+		$this->head->setNext(NULL);
+	}
+
+	function isEmpty(){
+		return $this->head->getNext()==NULL;
+	}
+
+	function length(){
+		$p=$this->head->getNext();
+		$length=0;
+		while($p!=NULL){
+			$p=$this->head->getNext();
+			$length+=1;
+		}
+		return $length;
+	}
+
+	function display(){
+		$p=$this->head->getNext();
+		while($p!=NULL){
+			print("data=".$this->head->getData()."<br>");
+			$p=$this->head->getNext();
+		}
 	}
 }
 
