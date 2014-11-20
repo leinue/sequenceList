6e0a0569948ec70f17fb2e55aeb7643bd882a0f7
ivy <597055914@qq.com>
2014-11-20 13:20:23 +0800

    update class linkQueue

---
+++ linkQueue.php
@@ -0,0 +1,47 @@
+<?php
+
+require("linklist.php"); //引入linkList中的node类
+
+/**
+* linkQueue
+*/
+class linkQueue{
+	
+	private $front;
+	private $rear;
+	private $queueElem=array();
+
+	function __construct(){
+
+	}
+
+	function clear(){
+
+	}
+
+	function length(){
+
+	}
+
+	function offer($x){
+
+	}
+
+	function poll(){
+
+	}
+
+	function peek(){
+
+	}
+
+	function isEmpty(){
+
+	}
+
+	function display(){
+
+	}
+}
+
+?>
