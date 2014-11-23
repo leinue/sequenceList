2889a2fc0913685b5d83ef541ea79a9344242001
ivy <597055914@qq.com>
2014-11-23 15:33:46 +0800

    二叉树先根遍历非递归算法

--- biTree.php
+++ biTree.php
@@ -1,5 +1,7 @@
 <?php
 
+require('linkStack.php');
+
 /**
 * biTreeNode
 * 二叉链表结构存储
@@ -60,6 +62,7 @@ class biTree{
 		$this->root=$new;
 	}
 
+	/*
 	//先根遍历递归算法
 	function preRootTraverse(biTreeNode $T){
 		if($T!=NULL){
@@ -72,20 +75,44 @@ class biTree{
 	//中根遍历递归算法
 	function inRootTraverse(biTreeNode $T){
 		if($T!=NULL){
-			preOrderTraverse($T->getLchild());
+			inOrderTraverse($T->getLchild());
 			print($T->getData());
-			preOrderTraverse($T->getRchild());
+			inOrderTraverse($T->getRchild());
 		}		
 	}
 
 	//后根遍历递归算法
 	function postRootTraverse(biTreeNode $T){
 		if($T!=NULL){
-			preOrderTraverse($T->getLchild());
-			preOrderTraverse($T->getRchild());
+			postOrderTraverse($T->getLchild());
+			postOrderTraverse($T->getRchild());
 			print($T->getData());
 		}		
-	}	
+	}*/
+
+	//先根非递归算法
+	function preRootTraverse(){
+		$T=$this->root;
+
+		while($T!=NULL){
+			$stack=new linkStack();
+			$stack->push($T);
+			while(!$stack->isEmpty()){
+				$T=$stack->pop();
+				print($T->getData());
+				while($T!=NULL){
+					if($T->getLchild()!=NULL){
+						print($T->getLchild()->getData());
+					}
+					if($T->getRchild()!=NULL){
+						print($T->getRchild()->getData());
+					}
+				}
+			}
+		}
+		
+
+	}
 
 }
 
