014ad93c45c39d38ed30bbfbfd2e8c1183ed6070
ivy <597055914@qq.com>
2014-11-23 14:18:33 +0800

    update class biTreeNode

---
+++ biTree.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+* biTreeNode
+* 二叉链表结构存储
+*/
+class biTreeNode{
+	
+	private $data;
+	private $lchild;
+	private $rchild;
+
+	function __construct($data=NULL,biTreeNode $lchild=NULL,biTreeNode $rchild=NULL){
+		$this->data=$data;
+		$this->lchild=$lchild;
+		$this->rchild=$rchild;
+	}
+
+	function getData(){
+		return $this->data;
+	}
+
+	function getLchild(){
+		return $this->lchild;
+	}
+
+	function getRchild(){
+		return $this->rchild;
+	}
+
+	function setData($new){
+		$this->data=$new;
+	}
+
+	function setLchild($new){
+		$this->lchild=$new;
+	}
+
+	function setRchild($new){
+		$this->rchild=$new;
+	}
+}
+
+?>
