49cb54628d96eae6b8f96d450fb9c815a2134cea
ivy <597055914@qq.com>
2014-11-25 23:26:58 +0800

    update function levelTraverse

--- tree.php
+++ tree.php
@@ -1,6 +1,82 @@
 <?php
 
 /**
+* linkQueue
+*/
+class linkQueue{
+	
+	private $front;
+	private $rear;
+
+	function __construct(){
+		$this->front=$this->rear=new node();
+	}
+
+	function clear(){
+		$this->front=$this->rear=NULL;
+	}
+
+	function length(){
+		if($this->isEmpty()){
+			return NULL;
+		}else{
+			$p=$this->front;
+			$length=0;
+			while($p!=NULL){
+				$p=$p->getNext();
+				$length++;
+			}
+			return $length-1;
+		}
+	}
+
+	function offer($x){
+		$p=new node($x);
+		if($this->front!=NULL){
+			$this->rear->setNext($p);
+			$this->rear=$p;
+		}else{
+			$this->fron=$this->rear=$p;
+		}
+	}
+
+	function poll(){
+		print($this->front->getData());
+		if($this->front!=NULL){
+			$p=$this->front;
+			$this->front=$this->front->getNext()->getNext();
+			return $p->getNext()->getData();
+		}else{
+			return NULL;
+		}
+	}
+
+	function peek(){
+		if($this->isEmpty()){
+			return NULL;
+		}else{
+			return $this->front->getNext()->getData();
+		}
+	}
+
+	function isEmpty(){
+		return $this->front==NULL;
+	}
+
+	function display(){
+		if($this->isEmpty()){
+			print("NULL");
+		}else{
+			$p=$this->front;
+			while($p!=NULL){
+				print($p->getData()."  ");
+				$p=$p->getNext();
+			}
+		}
+	}
+}
+
+/**
 * treeNode
 */
 class CSTreeNode{
@@ -54,6 +130,22 @@ function postRoorTraverse($T){
 		preRootTraverse($T->firstChild());
 		print($T->getData());
 		preRootTraverse($T->getNextSibling());		
+	}
+}
+
+//层次遍历
+function levelTraverse($T){
+	if($T!=NULL){
+		$l=new linkQueue();
+		$l->offer($T);
+		while(!$l->isEmpty()){
+			for($T=$l->poll();$T!=NULL;$T=$T->getNextSibling()){
+				print($T->getData()."   ");
+				if($T->getFirstChild()!=NULL){
+					$l->offer($T->getFirstChild());
+				}
+			}
+		}
 	}
 }
 
