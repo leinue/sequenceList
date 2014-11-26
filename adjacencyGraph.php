439db64991923290e593860afbb47728d55a4e35
ivy <597055914@qq.com>
2014-11-26 22:26:38 +0800

    update class ArcNode

--- adjacencyGraph.php
+++ adjacencyGraph.php
@@ -30,4 +30,44 @@ class VNode{
 	}
 }
 
+/**
+* 边结点类
+*/
+class arcNode{
+
+	private $value;//边或弧的权值
+	private $nextArc;//指向下一条弧
+	private $adjVex;//改弧指向的顶点位置
+	
+	function __construct($value=NULL,$nextArc=NULL,$adjVex=NULL){
+		$this->value=$value;
+		$this->adjVex=$adjVex;
+		$this->nextArc=$nextArc;
+	}
+
+	function getValue(){
+		return $this->value;
+	}
+
+	function getAjdVex(){
+		return $this->adjVex;
+	}
+
+	function getNextArc(){
+		return $this->nextArc;
+	}
+
+	function setValue($new){
+		$this->value=$new;
+	}
+
+	function setAdjVex($new){
+		$this->adjVex=$new;
+	}
+
+	function setNextArc($new){
+		$this->nextArc=$new;
+	}
+}
+
 ?>
