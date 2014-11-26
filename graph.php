3f585b65f0b7211097416f94ee5fadfdd7e92b70
ivy <597055914@qq.com>
2014-11-26 21:26:37 +0800

    fix function createUDN

--- graph.php
+++ graph.php
@@ -7,15 +7,17 @@ class MGraph{
 	
 	private $kind;//图的种类标志
 	private $vexNum,$arcNum;//图的当前顶点数和边数
-	private $vexs=array();//顶边 
+	private $vexs=array();//顶点
+	private $weight=array();//权值
 	private $arcs=array();//邻接矩阵
 
-	function __construct($kind,$vexNum,$arcNum,$vexs,$arcs){
+	function __construct($kind,$vexNum,$arcNum,$vexs,$weight,$arcs){
 		$this->kind=$kind;
 		$this->vexNum=$vexNum;
 		$this->arcNum=$arcNum;
 		$this->vexs=$vexs;
 		$this->arcs=$arcs;
+		$this->weight=$weight;
 	}
 
 	//创建图
@@ -51,7 +53,17 @@ class MGraph{
 
 	//创建无向网
 	function createUDN(){
+		/*for($v=0;$v<$this->vexNum;$v++){
+			for($u=0;$u<$vexNum;$u++){
+				$this->arcs[$v][$u]=2147483647;
+			}
+		}*/
 
+		for($k=0;$k<$this->arcNum;$k++){
+			$v=$this->locateVex($this->vexs[$k]);
+			$u=$this->locateVex($this->vexs[$k+1]);
+			$this->arcs[$v][$u]=$this->arcs[$u][$v]=$this->weight[$k];
+		}
 	}
 
 	//创建有向网
@@ -81,7 +93,12 @@ class MGraph{
 
 	//给定顶点的值vex,返回其在图中的位置,如果图中不包含此顶点则返回-1
 	function locateVex($vex){
-
+		for($v=0;$v<$this->vexNum;$v++){
+			if($this->vexs[$v]==$vex){
+				return $v;
+			}
+		}
+		return -1;
 	}
 
 	//返回v表示顶点的值
