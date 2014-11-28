13dd30283a8c7744b198bc09f792d5d2a95efe54
ivy <597055914@qq.com>
2014-11-28 13:42:19 +0800

    update function getMinMum

--- miniSpanTree_prim.php
+++ miniSpanTree_prim.php
@@ -1,5 +1,7 @@
 <?php
 
+require('graph.php');
+
 /*
 * 辅助记录从顶点集U到V-U的代价的最小的边
 */
@@ -19,8 +21,34 @@ class CloseEdge{
 */
 class miniSpanTree_PRIM{
 
-	function __construct(){
-		
+	private $closeEdge;
+
+	function __construct(CloseEdge $ce){
+		$this->closeEdge=$ce;
+	}
+
+	function PRIM(MGraph $G,$u){
+		$tree[$G->getVexNum()-1][2];
+		$count=0;
+		$this->closeEdge=new CloseEdge($G->getVexNum());
+		$k=$G->locateVex($u);
+		for($j=0;$j<$G->getVexNum();$j++){
+			if($j!=$k){
+				$this->closeEdge[$j]=new CloseEdge($u,$G->getArcs()[$k][$j]);
+			}
+		}
+		$this->closeEdge[$k]=new CloseEdge($u,0);
+	}
+
+	function getMinMum(){
+		$min=2147483647;
+		$v=-1;
+		for($i=0;$i<count($this->closeEdge);$i++){
+			if($this->closeEdge->lowCost!=0 && $this->closeEdge[$i]->lowCost<$min){
+				$min=$this->closeEdge[$i]->lowCost;
+				$v=$i;
+			}
+		}
 	}
 }
 
