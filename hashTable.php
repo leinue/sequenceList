a9bb081cd008510ece74735a66e6e296e2470e92
ivy <597055914@qq.com>
2014-12-03 19:51:31 +0800

    update class hashTable

---
+++ hashTable.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+* hash table
+*/
+class hashTable{
+	
+	private $table=array();
+
+	function __construct($size){
+		for($i=0;$i<$size;$i++){
+			$this->table[$i]=new linkList();
+		}
+	}
+
+	function hash($key){
+		return $key % count($this->table);
+	}
+
+	function insert($element){
+		$key=$this->hashCode($element);
+		$i=$this->hash($key);
+		$this->table[$i]->insert(0,$element);
+	}
+
+	function hashCode($ele){
+		return ord($ele);
+	}
+
+	function printHashTable(){
+		foreach($this->table as $key=>$value){
+			print("table $key =");
+			$value->display();
+		}
+	}
+
+	function search($element){
+		$key=$this->hashCode($element);
+		$i=$this->hash($key);
+		$index=$this->table[$i]->indexOf($element);
+		if($index>=0){
+			return $this->table[$i]->get($index);
+		}else{
+			return NULL;
+		}
+	}
+
+	function contain($element){
+		return $this->search($element)!=NULL;
+	}
+
+	function remove($element){
+		$key=$this->hashCode($element);
+		$i=$this->hash($key);
+		$index=$this->table[$i]->indexOf($element);
+		if($index>=0){
+			$this->table[$i]->remove($index);
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
